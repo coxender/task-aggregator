f7edaae6f481745c1055d0ebcef34bf96bfc8dd6
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2023-04-29 11:03:14 -0400

    feat: :poop: add listener and prevent default

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
+// prevent default submit
+// add listeners
+// file to and file from functions
 
-
-
+const form = document.querySelector("#task-form")!;
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // form get attributes
+});
+/*WHY IS THIS ANGRY 
+(event)->{
+  event.PreventDefault
+});*/

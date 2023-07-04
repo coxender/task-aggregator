22ed43783ff2c3757286a9c61f8e8062e0e502e1
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2023-07-03 22:33:50 -0400

    feat: ✨ add json schema
    Co-authored-by: clone 201 <coxender@users.noreply.github.com>

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,23 @@ export type Task = {
   tags: Tag[];
   description: string;
 };
+
+const JSON_SCHEMA = "https://coxender.github.io/task-aggregator/json-schema/tagg-v1.schema.json";
+
+export type TaggFile = {
+  $schema: typeof JSON_SCHEMA;
+  tags: Tag[];
+  tasks: Task[];
+};
+
+export function isValidTaggFile(json: unknown): json is TaggFile {
+  if (typeof json != "object" || json == null) {
+    return false;
+  }
+
+  if (!("$schema" in json) || json.$schema != JSON_SCHEMA) {
+    return false;
+  }
+
+  return true;
+}

60326eb69c48958232683f978eee846db9243158
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2023-11-11 16:07:21 -0500

    docs: 💡 add jsdoc comments to types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,18 @@
 export type Tag = {
+  /** The name of the tag. */
   name: string;
+  /** An srgb hexadecimal color e.g. `#ff00ff` */
   color: string;
 };
 
 export type Task = {
-  // uid is just the unix timestamp
+  /** The date of the string. */
   date: string;
+  /** The duration of the task in minutes. */
   minutes: number;
+  /** The names of the tags attached to a task. */
   tagNames: Tag["name"][];
+  /** The description of the task. */
   description: string;
 };
 
@@ -15,7 +20,9 @@ const JSON_SCHEMA = "https://coxender.github.io/task-aggregator/json-schema/tagg
 
 export type TaggFile = {
   $schema: typeof JSON_SCHEMA;
+  /** The list of current tags */
   tags: Tag[];
+  /** The past recorded tasks. */
   tasks: Task[];
 };
 

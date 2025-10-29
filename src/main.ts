d7267fe1e6ab087c406b79d51a5d90871996ade3
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2025-10-29 19:32:23 -0400

    refactor: :recycle: remove comments, organize main into regions

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import "./file";
 // add new tags
 // delete tags
 // file to and file from functions
-
+//#region SETUP
 let tasks: Task[] = [];
 let tags: Tag[] = [];
 let save: (tags: Tag[], tasks: Task[]) => void;
@@ -21,7 +21,23 @@ export function setup(newTags: Tag[], newTasks: Task[], newSave: (tags: Tag[], t
   resetTaskForm();
 }
 
+const dateInput = document.querySelector('#task-form [name="date"]') as HTMLInputElement;
+const hourInput = document.querySelector('#task-form [name="hours"]') as HTMLInputElement;
+const minuteInput = document.querySelector('#task-form [name="minutes"]') as HTMLInputElement;
+const descriptionBox = document.querySelector('#task-form [name="description"]') as HTMLTextAreaElement;
+const formWarningBox = document.querySelector("#form-warnings") as HTMLDivElement;
+
 const taskForm = document.querySelector("#task-form") as HTMLFormElement;
+const tasksListElement = document.querySelector("#tasks-list") as HTMLElement;
+
+const tagForm = document.querySelector("#tag-form") as HTMLFormElement;
+const tagsListElement = document.querySelector("#tags-list") as HTMLElement;
+const tagsDropdown = document.querySelector("#tag-dropdown") as HTMLElement;
+
+//#endregion
+//#region TASKS
+
+// create task
 taskForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -39,33 +55,6 @@ taskForm.addEventListener("submit", (event) => {
   // clear tags and description in form
   resetTaskForm();
 });
-
-const tagForm = document.querySelector("#tag-form") as HTMLFormElement;
-tagForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const data = new FormData(tagForm);
-  const name = data.get("name") as string;
-  const color = data.get("color") as string;
-
-  let errorText = document.querySelector("#tag-submission-error-text") as HTMLElement;
-  if (tags.some((tag) => tag.name === name)) {
-    errorText.innerHTML = "Cannot have duplicate tags!!";
-  } else {
-    errorText.innerHTML = "";
-    tagForm.reset();
-    tags.push({ name, color });
-    updateTags();
-  }
-});
-
-const tasksListElement = document.querySelector("#tasks-list") as HTMLElement;
-
-function displayDuration(totalMinutes: number): string {
-  const hour = getFormattedHours(totalMinutes);
-  const minutes = getFormattedMinutes(totalMinutes);
-  return `${hour} hr ${minutes < 10 ? "0" + minutes : minutes} min`;
-}
 
 function updateTasks() {
   let tagColors: { [name: string]: string } = Object.fromEntries(tags.map((tag) => [tag.name, tag.color]));
@@ -96,28 +85,6 @@ function updateTasks() {
   updateDailyTime();
 }
 
-const tagsListElement = document.querySelector("#tags-list") as HTMLElement;
-const tagsDropdown = document.querySelector("#tag-dropdown") as HTMLElement;
-function updateTags() {
-  const tagTemplate = tags.map(
-    (tag) =>
-      html`<div style="--tag-color: ${tag.color}">
-        <span>${tag.name}</span>
-        <button type="button" @click="${() => deleteTag(tag.name)}">X</button>
-      </div> `,
-  );
-  const optionsTemplate = tags.map((tag) => html`<option>${tag.name}</option>`);
-  render(tagTemplate, tagsListElement);
-  render(optionsTemplate, tagsDropdown);
-  save(tags, tasks);
-}
-
-const dateInput = document.querySelector('#task-form [name="date"]') as HTMLInputElement;
-const hourInput = document.querySelector('#task-form [name="hours"]') as HTMLInputElement;
-const minuteInput = document.querySelector('#task-form [name="minutes"]') as HTMLInputElement;
-const descriptionBox = document.querySelector('#task-form [name="description"]') as HTMLTextAreaElement;
-const formWarningBox = document.querySelector("#form-warnings") as HTMLDivElement;
-
 function resetTaskForm() {
   taskForm.reset();
   dateInput.valueAsDate = new Date();
@@ -142,15 +109,52 @@ function editTask(task: Task) {
   render(warningText, formWarningBox);
   updateTasks();
 }
+//#endregion
+
+//#region TAGS
+
+// create tags
+tagForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const data = new FormData(tagForm);
+  const name = data.get("name") as string;
+  const color = data.get("color") as string;
+
+  let errorText = document.querySelector("#tag-submission-error-text") as HTMLElement;
+  if (tags.some((tag) => tag.name === name)) {
+    errorText.innerHTML = "Cannot have duplicate tags!!";
+  } else {
+    errorText.innerHTML = "";
+    tagForm.reset();
+    tags.push({ name, color });
+    updateTags();
+  }
+});
+
+function updateTags() {
+  const tagTemplate = tags.map(
+    (tag) =>
+      html`<div style="--tag-color: ${tag.color}">
+        <span>${tag.name}</span>
+        <button type="button" @click="${() => deleteTag(tag.name)}">X</button>
+      </div> `,
+  );
+  const optionsTemplate = tags.map((tag) => html`<option>${tag.name}</option>`);
+  render(tagTemplate, tagsListElement);
+  render(optionsTemplate, tagsDropdown);
+  save(tags, tasks);
+}
 
 function deleteTag(name: string) {
   tags = tags.filter((item) => item.name != name);
   updateTags();
 }
+//#endregion
 
+//#region TIME
 function updateDailyTime() {
   const today: Date = new Date();
-  //const todayFormatted = `${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`;
   // get date in YYYY-MM-DD
   let todayFormatted: string = today.toISOString().split("T")[0];
   let todaysTasks = tasks.filter((task) => task.date == todayFormatted);
@@ -165,10 +169,18 @@ function updateDailyTime() {
 function getFormattedMinutes(totalMinutes: number) {
   return totalMinutes % 60;
 }
+
 function getFormattedHours(totalMinutes: number) {
   return Math.floor(totalMinutes / 60);
+}
+
+function displayDuration(totalMinutes: number): string {
+  const hour = getFormattedHours(totalMinutes);
+  const minutes = getFormattedMinutes(totalMinutes);
+  return `${hour} hr ${minutes < 10 ? "0" + minutes : minutes} min`;
 }
 
 function clearWarnings() {
   render(html``, formWarningBox);
 }
+//#endregion TIME
